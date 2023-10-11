7442ddf0361d79fbb613a26e928cd4826f2ffebb
Lucilia Seraidarian <lucilia.seraidarian@ps1it.com.br>
2023-10-11 14:39:43 -0300

    Datagrid: scroll horizontal e vertical

---
+++ app/control/datagrids/DatagridScrollVertical.php
@@ -0,0 +1,90 @@
+<?php
+
+use Adianti\Control\TPage;
+use Adianti\Widget\Container\TPanelGroup;
+use Adianti\Widget\Datagrid\TDataGrid;
+use Adianti\Widget\Datagrid\TDataGridAction;
+use Adianti\Widget\Datagrid\TDataGridColumn;
+use Adianti\Widget\Dialog\TMessage;
+use Adianti\Wrapper\BootstrapDatagridWrapper;
+
+class DatagridScrollVertical extends TPage
+{
+    private $datagrid;
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->datagrid = new BootstrapDatagridWrapper(new TDataGrid);
+        $this->datagrid->width = '100%';
+
+        $this->datagrid->setHeight('60vh');
+        $this->datagrid->makescrollable();
+        
+        $col_id         = new TDataGridColumn('id',         'Código',     'center');
+        $col_nome       = new TDataGridColumn('nome',       'Nome',       'left');
+        $col_nascimento = new TDataGridColumn('nascimento', 'Nascimento', 'left');
+        $col_cidade     = new TDataGridColumn('cidade',     'Cidade',     'left');
+        $col_estado     = new TDataGridColumn('estado',     'Estado',     'left');
+        $col_pais       = new TDataGridColumn('pais',       'País',       'left');
+        $col_telefone   = new TDataGridColumn('telefone',   'Telefone',   'left');
+        $col_email      = new TDataGridColumn('email',      'E-mail',     'left');
+
+        $this->datagrid->addColumn($col_id);
+        $this->datagrid->addColumn($col_nome);
+        $this->datagrid->addColumn($col_nascimento);
+        $this->datagrid->addColumn($col_cidade);
+        $this->datagrid->addColumn($col_estado);
+        $this->datagrid->addColumn($col_pais);
+        $this->datagrid->addColumn($col_telefone);
+        $this->datagrid->addColumn($col_email);
+
+        $action1 = new TDataGridAction([ $this, 'onView' ], [ 'id' => '{id}', 'nome' => '{nome}' ]);
+        $action2 = new TDataGridAction([ $this, 'onDelete' ], [ 'id' => '{id}', 'nome' => '{nome}' ]);
+
+        $this->datagrid->addAction( $action1, 'Visualiza', 'fa:search blue' );
+        $this->datagrid->addAction( $action2, 'Exclui', 'fa:trash red' );
+        
+        $this->datagrid->createModel();       
+
+        $panel = new TPanelGroup('Datagrid');
+        $panel->add($this->datagrid);
+        
+        parent::add($panel);
+    }
+
+    public static function onView($param)
+    {
+        new TMessage('info', 'ID: ' . $param['id'] . ' - nome: ' . $param['nome'] );
+    }
+
+    public static function onDelete($param)
+    {
+        new TMessage('error', 'ID: ' . $param['id'] . ' - nome: ' . $param['nome'] );
+    }
+    
+    public function onReload()
+    {
+        $this->datagrid->clear(); //garante que a datagrid esteja limpa antes de exibir os registros.
+
+        for ($n=1; $n <=40; $n++)
+        {
+            $item = new stdclass;
+            $item->id = $n;
+            $item->nome = 'Cliente ' . $n;            
+            $item->cidade = 'Cidade ' . $n;
+            $item->estado = 'Estado '. $n;
+            $item->pais = 'País '. $n;
+            $item->telefone = 'Telefone '. $n;
+            $item->email = 'email'. $n . '@gmail.com';
+            $this->datagrid->addItem($item);
+        }        
+    }
+
+    public function show()
+    {
+        $this->onReload();
+        parent::show();
+    }
+}
